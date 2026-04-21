313ec0d770c7a0c983a09ff3c06cbf57cff1aae1
Maxime <maximegp@hotmail.fr>
2026-04-21 20:45:06 +0100

    feature : Ajout de classe Utilisateur

--- back/src/main/java/back/entity/Utilisateur.java
+++ back/src/main/java/back/entity/Utilisateur.java
@@ -4,6 +4,4 @@ package back.entity;
 import jakarta.annotation.Nullable;
 
 public record Utilisateur(Integer id, @Nullable String identifiant, @Nullable String password, @Nullable String adresseMail) {
-
-
 }
