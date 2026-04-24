93b0926ff0d839b2c014a24c764d1b48810cff44
Maxime <maximegp@hotmail.fr>
2026-04-24 11:18:41 +0100

    feat : Ajout de la gestion d'erreur

--- front/src/pages/CreateUser.jsx
+++ front/src/pages/CreateUser.jsx
@@ -1,42 +1,86 @@
 import { useForm } from 'react-hook-form';
 import { createUser } from '../hooks/useUser';
+import { useNavigate } from 'react-router';
 
 const CreateUser = () => {
     const {
         register,
         handleSubmit,
-        reset,
+        setError,
         formState: { errors }
     } = useForm();
 
+    const navigate = useNavigate();
     const onSubmit = (data) => {
-        createUser(data.adresseMail,data.password,data.identifiant);
-        reset();
+        createUser(data.adresseMail, data.password, data.identifiant)
+            .then((data) => {
+                if (data) {
+                    navigate('/profil', { state: { user: data } });
+                } else {
+                    setError('createUser', {
+                        type: 'custom',
+                        message: data.message
+                    });
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                setError('createUser', {
+                    type: 'custom',
+                    message: error.message
+                });
+            });
     };
 
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input
-                    placeholder="Identifiant"
-                    {...register('identifiant', { required: true })}></input>
-                <label>
-                    {errors.identifiant && <span>Champ obligatoire</span>}
-                </label>
-                <input
-                    placeholder="Password"
-                    {...register('password', { required: true })}></input>
-                <label>
-                    {errors.password && <span>Champ obligatoire</span>}
-                </label>
-                <input
-                    placeholder="Adresse Mail"
-                    {...register('adresseMail', { required: true })}></input>
-                <label>
-                    {errors.adresseMail && <span>Champ obligatoire</span>}
-                </label>
+                <div className="flex_column profil_align_start margin5">
+                    <div className="flex_column margin5">
+                        <input
+                            placeholder="Identifiant"
+                            {...register('identifiant', {
+                                required: true
+                            })}></input>
+                        <label>
+                            {errors.identifiant && <span>Champ requis</span>}
+                        </label>
+                    </div>
+                    <div className="flex_column profil_align_start margin5">
+                        <input
+                            placeholder="Password"
+                            {...register('password', {
+                                required: true
+                            })}></input>
+                        <label>
+                            {errors.password && <span>Champ requis</span>}
+                        </label>
+                    </div>
 
-                <button type="submit">Valider</button>
+                    <div className="flex_column profil_align_start margin5">
+                        <input
+                            placeholder="Adresse Mail"
+                            {...register('adresseMail', {
+                                required: true,
+                                pattern: {
+                                    value: /^\S+@\S+\.\S+$/,
+                                    message: 'Email invalide'
+                                }
+                            })}></input>
+                    </div>
+                    <label>
+                        {errors.adresseMail && (
+                            <span>{errors.adresseMail.message}</span>
+                        )}
+                    </label>
+
+                    <button type="submit">Valider</button>
+                    <label>
+                        {errors.createUser && (
+                            <span> {errors.createUser.message}</span>
+                        )}
+                    </label>
+                </div>
             </form>
         </div>
     );
