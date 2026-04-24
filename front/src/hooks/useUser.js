1e9c30fb697f9dc411d1d5b5fd18703d928540a3
Kevin Parry <kevingp@hotmail.fr>
2026-04-24 15:02:52 +0200

    fix: finalisation endponit create user

--- front/src/hooks/useUser.js
+++ front/src/hooks/useUser.js
@@ -1,8 +1,5 @@
-import { post } from "../utils/api";
+import { post } from '../utils/api';
 
-
-export const createUser = async (adresseMail,password,identifiant) => {
-  
-    return post('user' , {adresseMail,password,identifiant})
- 
+export const createUser = async (adresseMail, password, identifiant) => {
+    return post('createUser', { adresseMail, password, identifiant });
 };
--- front/src/pages/CreateUser.jsx
+++ front/src/pages/CreateUser.jsx
@@ -1,20 +1,43 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { createUser } from '../hooks/useUser';
 import { useNavigate } from 'react-router';
+import { createUser } from '../hooks/useUser';
+import ic_eye from '../img/ic_eye.svg';
+import ic_eye_slash from '../img/ic_eye_slash.svg';
 
 const CreateUser = () => {
     const {
         register,
         handleSubmit,
         setError,
-        formState: { errors }
+        formState: { errors },
+        clearErrors
     } = useForm();
 
     const navigate = useNavigate();
+
+    const [passwordType, setPasswordType] = useState('password');
+
+    const showPassword = (event) => {
+        event.preventDefault();
+        if (passwordType === 'password') {
+            setPasswordType('text');
+            return;
+        }
+        setPasswordType('password');
+    };
+
+    const returnIconeEye = (typeInput) => {
+        if (typeInput === 'text') {
+            return <img src={ic_eye} alt="" />;
+        }
+        return <img src={ic_eye_slash} alt="" />;
+    };
+
     const onSubmit = (data) => {
         createUser(data.adresseMail, data.password, data.identifiant)
             .then((data) => {
-                if (data) {
+                if (data.id) {
                     navigate('/profil', { state: { user: data } });
                 } else {
                     setError('createUser', {
@@ -40,24 +63,35 @@ const CreateUser = () => {
                         <input
                             placeholder="Identifiant"
                             {...register('identifiant', {
-                                required: true
-                            })}></input>
+                                required: true,
+                                onChange: () => clearErrors('createUser')
+                            })}
+                        />
                         <label>
                             {errors.identifiant && <span>Champ requis</span>}
                         </label>
                     </div>
-                    <div className="flex_column profil_align_start margin5">
-                        <input
-                            placeholder="Password"
-                            {...register('password', {
-                                required: true
-                            })}></input>
+                    <div className="flex_column margin5">
+                        <div className="flex center">
+                            <input
+                                placeholder="Password"
+                                {...register('password', {
+                                    required: true,
+                                    onChange: () => clearErrors('createUser')
+                                })}
+                            />
+                            <button
+                                className="image_eye"
+                                onClick={showPassword}>
+                                {returnIconeEye(passwordType)}
+                            </button>
+                        </div>
                         <label>
                             {errors.password && <span>Champ requis</span>}
                         </label>
                     </div>
 
-                    <div className="flex_column profil_align_start margin5">
+                    <div className="flex_column margin5">
                         <input
                             placeholder="Adresse Mail"
                             {...register('adresseMail', {
@@ -65,8 +99,10 @@ const CreateUser = () => {
                                 pattern: {
                                     value: /^\S+@\S+\.\S+$/,
                                     message: 'Email invalide'
-                                }
-                            })}></input>
+                                },
+                                onChange: () => clearErrors('createUser')
+                            })}
+                        />
                     </div>
                     <label>
                         {errors.adresseMail && (
